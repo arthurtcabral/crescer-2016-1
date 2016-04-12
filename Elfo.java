e20379a6a7b851d4cc21e13eee173bda4c4189cc
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-04-12 14:06:16 -0300

    refactor(Elfo): diminuir repetição de código (variável vida)

--- Elfo.java
+++ Elfo.java
@@ -1,51 +1,46 @@
-
-public class Elfo extends Raca
-{
-    
-    protected int flechas;
-    private boolean acertar = true;
-    private Dwarf dwarf;
-    protected double vida = 100.0;
-    
-    public Elfo(String nome)
-    {
-        super(nome);
-        this.nome = nome;
-        this.flechas = 42;
-    }
-    
-    public Elfo(String nome, int experiencia)
-    {
-        super(nome, experiencia);
-        this.flechas = 42;
-    }
-    
-    public Elfo(String nome, int experiencia, int flechas){
-        super(nome, experiencia);
-        this.flechas = flechas;
-    }
+    public class Elfo extends Raca { 
       
-    public void atirarFlecha(Dwarf dwarf){
-        this.experiencia++;
-        this.flechas--;
-        dwarf.perdeVida();
-    }
-  
-    public int getFlechas(){
-    return this.flechas;
-   }
-       
-    public void setVida(double vida){
-        this.vida = vida;
-    }
+     protected int flechas = 42; 
+     private boolean acertar = true; 
+     private Dwarf dwarf; 
+     protected double vida = 100.0; 
+      
+     public Elfo(String nome) 
+     { 
+         super(nome);
+     } 
+      
+     public Elfo(String nome, int experiencia) 
+     { 
+         super(nome, experiencia); 
+     } 
+      
+     public Elfo(String nome, int experiencia, int flechas){ 
+         super(nome, experiencia); 
+         this.flechas = flechas; 
+     } 
+        
+     public void atirarFlecha(Dwarf dwarf){ 
+         this.experiencia++; 
+         this.flechas--; 
+         dwarf.perdeVida(); 
+     } 
     
-    public double getVida(){
-        return this.vida;
-    }
-    
-    public String toString(){
-    return this.nome + " possui " + this.flechas + " flechas e " + this.experiencia + " níveis de experiência.";
-    }
-    
-    
-}
+     public int getFlechas(){ 
+     return this.flechas; 
+    } 
+         
+     public void setVida(double vida){ 
+         this.vida = vida; 
+     } 
+      
+     public double getVida(){ 
+         return this.vida; 
+     } 
+      
+     public String toString(){ 
+     return this.nome + " possui " + this.flechas + " flechas e " + this.experiencia + " níveis de experiência."; 
+    } 
+      
+      
+ } 
