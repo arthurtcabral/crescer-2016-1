4cb6e101e9832872a558f5db470967bbc2ba9741
arthurtcabral <arthurtcabral@gmail.com>
2016-04-13 20:36:39 -0300

    feat(ExercitoDeElfos): adicionar método buscarPorStatus, que retorna um ArrayList<> de Elfos

--- ExercitoDeElfos.java
+++ ExercitoDeElfos.java
@@ -14,12 +14,40 @@ public class ExercitoDeElfos {
    }
    
    public void agruparPorStatus(){ // Criar ArrayList, pois os Status são de mesma chave(VIVO)
-           ArrayList<Elfo> listaElfos = new ArrayList<>();
+           ArrayList<Elfo> listaElfosVivos = new ArrayList<>();
+           ArrayList<Elfo> listaElfosMortos = new ArrayList<>();
            for(Elfo elfoAux : this.exercitoDeElfos.values()){
-           listaElfos.add(elfoAux);
-           this.agrupadoPorStatus.put(elfoAux.getStatus(), listaElfos);
+               if(elfoAux.getStatus().equals(Status.VIVO)){
+                   listaElfosVivos.add(elfoAux);
+                   this.agrupadoPorStatus.put(elfoAux.getStatus(), listaElfosVivos);
+               }else if(elfoAux.getStatus().equals(Status.MORTO)){
+                   listaElfosMortos.add(elfoAux);
+                   this.agrupadoPorStatus.put(elfoAux.getStatus(), listaElfosMortos);
+               }     
            }       
    }
+   
+   public ArrayList<Elfo> buscarPorStatus(Status status){
+    ArrayList<ArrayList<Elfo>> listaDeListasDeElfos = new ArrayList<>();
+    ArrayList<Elfo> listaElfosRetorno = new ArrayList<>();
+     for(ArrayList<Elfo> listaElfosAux : this.agrupadoPorStatus.values()){
+               listaDeListasDeElfos.add(listaElfosAux);
+     }
+     
+     if(listaDeListasDeElfos.get(0).get(0).getStatus().equals(status)){
+        for(Elfo elfoAux : listaDeListasDeElfos.get(0)){
+            listaElfosRetorno.add(elfoAux);
+        }
+        
+        }else if(listaDeListasDeElfos.get(1).get(0).getStatus().equals(status)){
+            for(Elfo elfoAux : listaDeListasDeElfos.get(1)){
+            listaElfosRetorno.add(elfoAux);
+            }
+        
+        }
+     
+     return listaElfosRetorno;
+    }
    
    public HashMap getExercitoDeElfos(){
    return this.exercitoDeElfos;
