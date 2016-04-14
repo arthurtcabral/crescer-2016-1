11cee0a140ad8f9a6c2d567b3d7bb89c151675f4
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-04-14 17:23:07 -0300

    refactor(DicionarioSindarin, SindarinParaIngles, SindarinParaPortugues): permitir a tradução de qualquer palavra pré-adicionada

--- DicionarioSindarin.java
+++ DicionarioSindarin.java
@@ -1,6 +1,37 @@
 import java.util.*;
 public class DicionarioSindarin {
-    public static void main(String[] args){       
+    
+    public static void main(String[] args){
+    Scanner teclado = new Scanner(System.in);
+    
+    System.out.println("Bem vindo ao tradutor Sindarin");
+    System.out.println("[P]ortuguês [I]nglês");
+    String idioma = teclado.nextLine();
+    TradutorSindarin tradutor = null;
+    switch(idioma.toLowerCase()){
+    case "i":
+        tradutor = new SindarinParaIngles();
+        break;
+    default:
+        tradutor = new SindarinParaPortugues();
+        break;
+    }
+    System.out.println("Escolha uma palavra abaixo: ");
+    System.out.println(" ");
+    System.out.println("amar");
+    System.out.println("naur");
+    System.out.println("gwaew");
+    System.out.println("nen");
+    System.out.println("gûr");
+    String palavra = teclado.nextLine();
+    if(palavra.equals("amar") || palavra.equals("naur") ||
+    palavra.equals("gwaew") || palavra.equals("nen") || palavra.equals("gûr")){
+    System.out.println(palavra + " significa " + tradutor.traduzir(palavra));
+    }
+    // System.out.println("Naur significa " + tradutor.traduzir("naur"));
+    }
+    
+    public static void rodarExemplosHashMap(){       
         HashMap<String, String> dicionarioSindarin = new HashMap<>();
         dicionarioSindarin.put("terra", "amar");
         dicionarioSindarin.put("fogo", "naur");
--- SindarinParaPortugues.java
+++ SindarinParaPortugues.java
@@ -4,8 +4,11 @@ public class SindarinParaPortugues implements TradutorSindarin{
     
     private HashMap<String, String> dicionario = new HashMap<String, String>(){
         {
+        put("amar", "terra");
         put("naur", "fogo");
-        
+        put("gwaew", "vento");
+        put("nen", "água");
+        put("gûr", "coração");
     }
     };
     
