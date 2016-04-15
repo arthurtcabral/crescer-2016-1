395cf1903cbd24eb5ffccc98498b9ffe32f0e65f
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-04-15 16:28:23 -0300

    test(ArteDaGuerraTest) testar ataque só com elfos verdes e testar ataque só com elfos noturnos

--- ArteDaGuerraTest.java
+++ ArteDaGuerraTest.java
@@ -6,19 +6,58 @@ import org.junit.Test;
 
 public class ArteDaGuerraTest {
     
+    @After
+    public void tearDown(){
+    System.gc();            
+    }
+    
     @Test
     public void listarElfosQueAtacaram(){
         ArrayList<Dwarf> listaDwarves = new ArrayList<Dwarf>();
         listaDwarves.add(new Dwarf("Hach"));
         listaDwarves.add(new Dwarf("Hech"));
         ExercitoDeElfos exercito = new ExercitoDeElfos();
-        exercito.alistarElfoNoExercitoSemRestricoes(new ElfosVerdes("Elfo Verde 1"));
-        exercito.alistarElfoNoExercitoSemRestricoes(new ElfosNoturnos("Elfo Noturno 1"));
-        exercito.alistarElfoNoExercitoSemRestricoes(new ElfosNoturnos("Elfo Noturno 2"));
-        exercito.alistarElfoNoExercitoSemRestricoes(new ElfosNoturnos("Elfo Noturno 3"));
+        exercito.alistarElfoNoExercito(new ElfosVerdes("Elfo Verde 1"));
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 1"));
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 2"));
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 3"));
         
         Estrategias estrategia = new ArteDaGuerra();
         ArrayList<Elfo> elfosQueAtacaram = estrategia.estrategia(listaDwarves, exercito);
         assertTrue(elfosQueAtacaram.size() == 2);
+    }
+    
+    @Test
+    public void ataqueSoComVerdes(){
+        ArrayList<Dwarf> listaDwarves = new ArrayList<Dwarf>();
+        listaDwarves.add(new Dwarf("Hach"));
+        listaDwarves.add(new Dwarf("Hech"));
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.alistarElfoNoExercito(new ElfosVerdes("Elfo Verde 1"));
+        exercito.alistarElfoNoExercito(new ElfosVerdes("Elfo Verde 2"));
+        exercito.alistarElfoNoExercito(new ElfosVerdes("Elfo Verde 3"));
+        exercito.alistarElfoNoExercito(new ElfosVerdes("Elfo Verde 4"));
+        
+        
+        Estrategias estrategia = new ArteDaGuerra();
+        ArrayList<Elfo> elfosQueAtacaram = estrategia.estrategia(listaDwarves, exercito);
+        assertTrue(elfosQueAtacaram.size() == 4);
+    }
+    
+    @Test
+    public void ataqueSoComNoturnos(){
+        ArrayList<Dwarf> listaDwarves = new ArrayList<Dwarf>();
+        listaDwarves.add(new Dwarf("Hach"));
+        listaDwarves.add(new Dwarf("Hech"));
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 1"));
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 2"));
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 3"));
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 4"));
+        
+        
+        Estrategias estrategia = new ArteDaGuerra();
+        ArrayList<Elfo> elfosQueAtacaram = estrategia.estrategia(listaDwarves, exercito);
+        assertTrue(elfosQueAtacaram.size() == 1);
     }
 }
