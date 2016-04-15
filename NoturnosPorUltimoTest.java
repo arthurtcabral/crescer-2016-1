7bf7644f33fcae61a366ed1c2df7a14045204f88
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-04-15 16:46:37 -0300

    test(NoturnosPorUltimoTest): testar atacar() só com elfos verdes e testar atacar() só com elfos noturnos

--- NoturnosPorUltimoTest.java
+++ NoturnosPorUltimoTest.java
@@ -17,17 +17,48 @@ public class NoturnosPorUltimoTest {
         listaDwarves.add(new Dwarf("Hach"));
         listaDwarves.add(new Dwarf("Hech"));
         ExercitoDeElfos exercito = new ExercitoDeElfos();
-        exercito.alistarElfoNoExercitoSemRestricoes(new ElfosVerdes("Elfo Verde 1"));
-        exercito.alistarElfoNoExercitoSemRestricoes(new ElfosNoturnos("Elfo Noturno 1"));
-        exercito.alistarElfoNoExercitoSemRestricoes(new ElfosNoturnos("Elfo Noturno 2"));
-        exercito.alistarElfoNoExercitoSemRestricoes(new ElfosNoturnos("Elfo Noturno 3"));
-        exercito.alistarElfoNoExercitoSemRestricoes(new Elfo("Elfo Comum 1"));
+        exercito.alistarElfoNoExercito(new ElfosVerdes("Elfo Verde 1"));
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 1"));
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 2"));
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 3"));
         
         Estrategias estrategia = new NoturnosPorUltimo();
-        ArrayList<Elfo> elfosQueAtacaram = estrategia.estrategia(listaDwarves, exercito);
+        ArrayList<Elfo> elfosQueAtacaram = estrategia.atacar(listaDwarves, exercito);
         assertTrue(elfosQueAtacaram.get(0).getNome().equals("Elfo Verde 1"));
-        assertTrue(elfosQueAtacaram.get(1).getNome().equals("Elfo Comum 1"));
-        assertTrue(elfosQueAtacaram.get(4).getNome().contains("Elfo Noturno"));
+        assertTrue(elfosQueAtacaram.get(3).getNome().contains("Elfo Noturno"));
     }
     
+    @Test
+    public void checarListagemSoComElfosVerdes(){
+        ArrayList<Dwarf> listaDwarves = new ArrayList<Dwarf>();
+        listaDwarves.add(new Dwarf("Hach"));
+        listaDwarves.add(new Dwarf("Hech"));
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.alistarElfoNoExercito(new ElfosVerdes("Elfo Verde 1"));
+        exercito.alistarElfoNoExercito(new ElfosVerdes("Elfo Verde 2"));
+        exercito.alistarElfoNoExercito(new ElfosVerdes("Elfo Verde 3"));
+        
+        Estrategias estrategia = new NoturnosPorUltimo();
+        ArrayList<Elfo> elfosQueAtacaram = estrategia.atacar(listaDwarves, exercito);
+        assertTrue(elfosQueAtacaram.get(0).getNome().equals("Elfo Verde 1"));
+        assertTrue(elfosQueAtacaram.get(1).getNome().equals("Elfo Verde 2"));
+        assertTrue(elfosQueAtacaram.get(2).getNome().equals("Elfo Verde 3"));
+    }
+    
+    @Test
+    public void checarListagemSoComElfosNoturnos(){
+        ArrayList<Dwarf> listaDwarves = new ArrayList<Dwarf>();
+        listaDwarves.add(new Dwarf("Hach"));
+        listaDwarves.add(new Dwarf("Hech"));
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 1"));
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 2"));
+        exercito.alistarElfoNoExercito(new ElfosNoturnos("Elfo Noturno 3"));
+        
+        Estrategias estrategia = new NoturnosPorUltimo();
+        ArrayList<Elfo> elfosQueAtacaram = estrategia.atacar(listaDwarves, exercito);
+        assertTrue(elfosQueAtacaram.get(0).getNome().contains("Noturno"));
+        assertTrue(elfosQueAtacaram.get(1).getNome().contains("Noturno"));
+        assertTrue(elfosQueAtacaram.get(2).getNome().contains("Noturno"));
+    }
 }
