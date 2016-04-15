53136aeb83544a2ebf0f04af78e19b6df691835c
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-04-15 14:49:05 -0300

    test(ArteDaGuerraTest): testar alteração no método arteDaGuerra

--- ArteDaGuerraTest.java
+++ ArteDaGuerraTest.java
@@ -12,13 +12,13 @@ public class ArteDaGuerraTest {
         listaDwarves.add(new Dwarf("Hach"));
         listaDwarves.add(new Dwarf("Hech"));
         ExercitoDeElfos exercito = new ExercitoDeElfos();
-        exercito.alistarElfoNoExercitoSemRestricoes(new Elfo("Elfo"));
+        exercito.alistarElfoNoExercitoSemRestricoes(new ElfosVerdes("Elfo Verde 1"));
         exercito.alistarElfoNoExercitoSemRestricoes(new ElfosNoturnos("Elfo Noturno 1"));
         exercito.alistarElfoNoExercitoSemRestricoes(new ElfosNoturnos("Elfo Noturno 2"));
         exercito.alistarElfoNoExercitoSemRestricoes(new ElfosNoturnos("Elfo Noturno 3"));
         
         Estrategias estrategia = new ArteDaGuerra();
         ArrayList<Elfo> elfosQueAtacaram = estrategia.estrategia(listaDwarves, exercito);
-        assertTrue(elfosQueAtacaram.size() == 3);
+        assertTrue(elfosQueAtacaram.size() == 2);
     }
 }
