25e459039ce9cd37040b43988c624dcae4f8b5ea
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-04-20 17:26:36 -0300

    EM ANDAMENTO

--- CursoSQL.sql
+++ CursoSQL.sql
@@ -1,189 +1,168 @@
-Create table Cidade 
-(
-  IDCidade  int         not null,
-  Nome      varchar(30) not null,
-  UF        varchar(2)  not null,
-    constraint PK_Cidade primary key (IDCidade)
-);	
-
-Create table Associado 
-(
-  IDAssociado    int         not null,
-  Nome           varchar(50) not null,
-  DataNascimento datetime    not null,
-  Sexo           char(1)     not null,
-  CPF            varchar(11) ,
-  Endereco       varchar(35),
-  Bairro         varchar(25),
-  Complemento    varchar(20),
-  IDCidade       int,
-     constraint PK_Associado primary key (IDAssociado),
-	 constraint FK_Associado foreign key (IDCidade)
-	    references Cidade (IDCidade)
+create table Empregado (
+ IDEmpregado    int not null
+,NomeEmpregado  varchar(30) not null
+,Cargo          varchar(15) not null
+,IDGerente      int null
+,DataAdmissao   datetime not null
+,Salario        decimal(7,2) not null
+,Comissao       decimal(7,2)
+,IDDepartamento int
 );
 
-insert into Cidade (IDCidade, Nome, UF)
-   values (1, 'Sao Leopoldo', 'RS');
-insert into Cidade (IDCidade, Nome, UF)
-   values (2, 'Porto Alegre', 'RS');
-insert into Cidade (IDCidade, Nome, UF)
-   values (3, 'Parobe', 'RS');
-insert into Cidade (IDCidade, Nome, UF)
-   values (4, 'Taquara', 'RS');
-insert into Cidade (IDCidade, Nome, UF)
-   values (105, 'São Paulo', 'SP');
-insert into Cidade (IDCidade, Nome, UF)
-   values (106, 'Guarulhos', 'SP');
-insert into Cidade (IDCidade, Nome, UF)
-   values (27, 'Rio de Janeiro', 'RJ');
-insert into Cidade (IDCidade, Nome, UF)
-   values (108, 'Brasilia', 'DF');
-insert into Cidade (IDCidade, Nome, UF)
-   values (109, 'Belo Horizonte', 'MG');
-insert into Cidade (IDCidade, Nome, UF)
-   values (110, 'Uberlândia', 'MG');
-insert into Cidade (IDCidade, Nome, UF)
-   values (5, 'Cidade de Alagoas', 'AL');
-insert into Cidade (IDCidade, Nome, UF)
-   values (6, 'Maceio', 'AL');
-insert into Cidade (IDCidade, Nome, UF)
-   values (7, 'Itu', 'SP');
-insert into Cidade (IDCidade, Nome, UF)
-   values (8, 'Campinas', 'SP');
-insert into Cidade (IDCidade, Nome, UF)
-   values (9, 'Guaratingueta', 'SP');
-insert into Cidade (IDCidade, Nome, UF)
-   values (10, 'Santa Barbara', 'MG');
-insert into Cidade (IDCidade, Nome, UF)
-   values (11, 'Campos dos Goitacases', 'RJ');
-insert into Cidade (IDCidade, Nome, UF)
-   values (12, 'Sao Gabriel', 'RS');
-insert into Cidade (IDCidade, Nome, UF)
-   values (13, 'Brasopolis', 'MG');
-insert into Cidade (IDCidade, Nome, UF)
-   values (14, 'Cristina', 'MG');
-insert into Cidade (IDCidade, Nome, UF)
-   values (15, 'Umbuzeiro', 'PB');
-insert into Cidade (IDCidade, Nome, UF)
-   values (16, 'Vicosa', 'MG');
-insert into Cidade (IDCidade, Nome, UF)
-   values (17, 'Macae', 'RJ');
-insert into Cidade (IDCidade, Nome, UF)
-   values (18, 'Sao Luis', 'MA');
-insert into Cidade (IDCidade, Nome, UF)
-   values (19, 'Cuiaba', 'MT');
-insert into Cidade (IDCidade, Nome, UF)
-   values (20, 'Sao Borja', 'RS');
-insert into Cidade (IDCidade, Nome, UF)
-   values (21, 'Natal', 'RN');
-insert into Cidade (IDCidade, Nome, UF)
-   values (22, 'Diamantina', 'MG');
-insert into Cidade (IDCidade, Nome, UF)
-   values (23, 'Campo Grande', 'MS');
-insert into Cidade (IDCidade, Nome, UF)
-   values (24, 'Fortaleza', 'CE');
-insert into Cidade (IDCidade, Nome, UF)
-   values (25, 'Bage', 'RS');
-insert into Cidade (IDCidade, Nome, UF)
-   values (26, 'Bento Goncalves', 'RS');
-insert into Cidade (IDCidade, Nome, UF)
-   values (28, 'Pinheiro', 'MA');
-insert into Cidade (IDCidade, Nome, UF)
-   values (29, 'Salvador', 'BA');
-insert into Cidade (IDCidade, Nome, UF)
-   values (30, 'Garanhuns', 'PE');
-Insert into Cidade (IDCidade, Nome, UF)
-   values (31, 'São Paulo', 'SP');    
-Insert into Cidade (IDCidade, Nome, UF)
-   values (32, 'Guarulhos', 'SP');    
-Insert into Cidade (IDCidade, Nome, UF)
-   values (33, 'Rio de Janeiro', 'RJ');    
-Insert into Cidade (IDCidade, Nome, UF)
-   values (34, 'Brasilia', 'DF');    
-Insert into Cidade (IDCidade, Nome, UF)
-   values (35, 'Belo Horizonte', 'MG');        
-Insert into Cidade (IDCidade, Nome, UF)
-   values  (36, 'Uberlândia', 'MG');      
-Insert into Cidade (IDCidade, Nome, UF)
-   values  (37, 'Santana do Livramento', 'RS');
-Insert into Cidade (IDCidade, Nome, UF)
-   values  (38, 'Santa Maria', 'RS');
-Insert into Cidade (IDCidade, Nome, UF)
-   values  (39, 'Santa Rosa', 'RS');
-Insert into Cidade (IDCidade, Nome, UF)
-   values  (40, 'São Francisco de Paula', 'RS');    
+insert into Empregado values (7369 ,'SMITH', 'Atendente', '7902', convert(datetime, '1980/12/17', 111), 800, null, 20);
+insert into Empregado values (7499 ,'ALLEN', 'Vendedor', '7698', convert(datetime, '1981/02/20', 111), 1600, 300, 30);
+insert into Empregado values (7521 ,'WARD', 'Vendedor', '7698', convert(datetime, '1981/02/22', 111), 1250, 500, 30);
+insert into Empregado values (7566 ,'JONES', 'Gerente', '7839', convert(datetime, '1981/04/02', 111), 2975, null, 20);
+insert into Empregado values (7654 ,'MARTIN', 'Vendedor', '7698', convert(datetime, '1981/09/28', 111), 1250, 1400, 30);
+insert into Empregado values (7698 ,'BLAKE', 'Gerente', '7839', convert(datetime, '1981/05/01', 111), 2850, null, 30);
+insert into Empregado values (7782 ,'CLARK', 'Gerente', '7839', convert(datetime, '1981/06/09', 111), 2450, null, 10);
+insert into Empregado values (7788 ,'SCOTT', 'Analista', '7566', convert(datetime, '1982/12/09', 111), 3000, null, 20);
+insert into Empregado values (7839 ,'KING', 'Presidente', null, convert(datetime, '1981/11/17', 111), 5000, null, null);
+insert into Empregado values (7844 ,'TURNER', 'Vendedor', '7698', convert(datetime, '1981/09/08', 111), 1500, 0, 30);
+insert into Empregado values (7876 ,'ADAMS', 'Atendente', '7788', convert(datetime, '1983/01/12', 111), 1100, null, 20);
+insert into Empregado values (7900 ,'JAMES', 'Atendente', '7698', convert(datetime, '1981/12/03', 111), 950, null, 30);
+insert into Empregado values (7902 ,'FORD', 'Analista', '7566', convert(datetime, '1981/12/03', 111), 3000, null, 20);
+insert into Empregado values (7934 ,'MILLER', 'Atendente', '7782', convert(datetime, '1982/01/23', 111), 1300, null, 10);
+insert into Empregado values (7940 ,'ANDREW', 'Atendente', '7782', convert(datetime, '1988/01/20', 111), 1150, null, null);
 
--- ASSOCIADOS...
-insert into Associado 
-      (IDAssociado, 
-       Nome, 
-       DataNascimento, 
-       Sexo)
-  values 
-      (1, 
-       'Ana Paula Padrao', 
-       convert(datetime, '10/05/1980', 103), 
-       'F' );
---
-insert into Associado 
-      (IDAssociado, 
-       Nome, 
-       DataNascimento, 
-       Sexo)
-  values  
-      (2, 
-       'Joao Soares', 
-       convert(datetime, '20/02/1980', 103), 
-       'M' );
+create table Departamento (
+ IDDepartamento   int not null
+,NomeDepartamento varchar(30)
+,Localizacao      varchar(25)
+);
+
+insert into Departamento values (10,'Contabilidade', 'SAO PAULO');
+insert into Departamento values (20,'Pesquisa', 'SAO LEOPOLDO');
+insert into Departamento values (30,'Vendas', 'SAO PAULO');
+insert into Departamento values (40,'Operações', 'RECIFE');
+insert into Departamento values (1, 'Presidência', 'RIBEIRAO PRETO');
+insert into Departamento values (60, 'Tecnologia', 'PORTO ALEGRE');
+
+---------
+ALTER TABLE Departamento ADD  CONSTRAINT PK_departamento 
+  PRIMARY KEY  
+  (IDDepartamento);
+
+ALTER TABLE Empregado ADD  CONSTRAINT PK_Empregado
+  PRIMARY KEY  
+  (IDEmpregado);
+
+ALTER TABLE Empregado ADD CONSTRAINT FK_Empregado_Departamento
+  FOREIGN KEY (IDDepartamento) REFERENCES Departamento (IDDepartamento);
+
+CREATE INDEX IX_Empregado_Departamento ON Empregado (IDDepartamento);
+
+SELECT 
+NomeEmpregado	as	Nome,
+salario			as	SalarioMensal,
+(salario*12)	as	SalarioAnual
+FROM Empregado;
+
+SELECT
+IDEmpregado, 
+NomeEmpregado	as	Nome,
+salario			as	SalarioMensal,
+(salario*12)	as	SalarioAnual
+FROM Empregado
+WHERE IDEmpregado in (7369, 7499, 7566);
+
+SELECT
+IDEmpregado, 
+NomeEmpregado	as	Nome,
+salario			as	SalarioMensal,
+(salario*12)	as	SalarioAnual
+FROM Empregado
+WHERE IDEmpregado = 7369
+OR IDEmpregado = 7499
+OR IDEmpregado = 7566;
+
+SELECT
+IDEmpregado, 
+NomeEmpregado	as	Nome,
+salario			as	SalarioMensal,
+(salario*12)	as	SalarioAnual
+FROM Empregado
+WHERE IDEmpregado BETWEEN 1000 AND 8000
+AND Salario > 2000;
+
+SELECT * FROM Empregado;
 
 SELECT * FROM Cidade;
+
+SELECT NomeEmpregado			   --Exercício 1
+FROM Empregado
+ORDER BY DataAdmissao ASC;
+
+SELECT NomeEmpregado, Salario	   --Exercício 2
+FROM Empregado
+WHERE Cargo = 'Atendente'
+OR Salario < 18.500;
+
+SELECT IDCidade					   --Exercício 3	
+FROM Cidade
+WHERE Nome = 'Uberlândia';
+
+SELECT IDCidade, Nome as Municipio --Exercício 4
+FROM Cidade
+WHERE UF = 'RS';
+
+SELECT COUNT(IDEmpregado) NumeroEmpregados
+FROM Empregado;
+
+SELECT COUNT(Comissao) -- Contou todas as linhas Comissao que possuiam valor
+FROM Empregado;
+
+SELECT 
+MAX(salario) MaiorSalario,
+MIN(salario) MenorSalario 
+FROM Empregado;
+
+Select Cargo, 
+COUNT(1) as Total 
+From Empregado 
+Group By Cargo;
+
+SELECT NomeEmpregado,
+Salario,
+ISNULL(Comissao, 0) as Comissao,			-- Se for null, atribui 0 a Comissao
+(Salario + ISNULL(Comissao, 0)) as Total_Salario
+From Empregado;
+
+SELECT Nome,
+REPLACE(Nome, 'A', 'É') primeiro_replace,
+UF
+FROM Cidade;
+
+select IDAssociado, Nome, 
+Case when sexo = 'F' then 'Feminino' 
+when sexo = 'M' then 'Masculino' 
+else 'sei lá' End as Genero from associado;
+
+Select SUBSTRING(Nome, 1, 3) as NomeCidade, UF From CIDADE Order by NomeCidade;
+
+SELECT SUBSTRING(Nome, 1, CHARINDEX(' ', Nome, 1) - 1) as Nome FROM Associado Order By Nome ASC; --Exercício 1
+
+SELECT																							 --Exercício 2
+Nome,
+DATEDIFF(Year, DataNascimento, GETDATE()) as Idade 
+From Associado;
+
+SELECT																							 --Exercício 3
+NomeEmpregado,
+DATEDIFF(MONTH, DataAdmissao, GETDATE()) as MesesDeTrabalho
+FROM Empregado
+Where DataAdmissao BETWEEN ('1980-05-01') AND ('1982-01-20');
+
+SELECT TOP 1 Cargo, COUNT(1) as CargoComMaisEmpregados											 --Exercício 4
+FROM Empregado
+GROUP BY Cargo
+ORDER BY CargoComMaisEmpregados DESC;
 
 SELECT * FROM Associado;
 
-SELECT * INTO CopiaCidade FROM Cidade;
-
-insert into Associado 
-      (IDAssociado, 
-       Nome, 
-       DataNascimento, 
-       Sexo,
-	   CPF)
-  values  
-      (3, 
-       'Julio de Castilhos', 
-       convert(datetime, '14/12/1947', 103), 
-       'M', '21234567895');
-
-insert into Associado 
-      (IDAssociado, 
-       Nome, 
-       DataNascimento, 
-       Sexo,
-	   CPF)
-  values  
-      (4, 
-       'Antonio Augusto Borges de Medeiros', 
-       convert(datetime, '19/03/1942', 103), 
-       'M', '81234567891');
-
-insert into Associado 
-      (IDAssociado, 
-       Nome, 
-       DataNascimento, 
-       Sexo,
-	   CPF)
-  values  
-      (5, 
-       'Osvaldo Aranha', 
-       convert(datetime, '08/02/1958', 103), 
-       'M', '01234567893');
-
-SELECT * INTO CidadeAux From Cidade;
-
-Truncate table CidadeAux;
-
-INSERT INTO CidadeAux (IDCidade, Nome, UF)
-SELECT * FROM Cidade;
-
-SELECT * FROM CidadeAux;
+SELECT																							 --Exercício 6
+Nome,
+DATEADD(YEAR, 50, DataNascimento) as DataQueCompletou50Anos,
+DATEPART(WEEKDAY, DATEADD(YEAR, 50, DataNascimento)) as DiaDaSemana
+FROM Associado;
 
