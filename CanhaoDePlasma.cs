529ba5609a45225bfabb89e8cc66a13d1d44e47d
Arthur Tassinari Cabral <Arthur Tassinari Cabral>
2016-05-07 18:17:10 -0300

    feat(BotasDeSuperVelocidade.cs, CanhaoDePlasma.cs, EscudoDeEnergia.cs, IUpgrade.cs): Adicionar novas classes para se adequar ao pedido do cliente vide nível 2.

---
+++ CanhaoDePlasma.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegamanLista1
+{
+    public class CanhaoDePlasma : IUpgrade
+    {
+        public int BonusAtaque
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public int BonusDefesa
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ EscudoDeEnergia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegamanLista1
+{
+    public class EscudoDeEnergia : IUpgrade
+    {
+        public int BonusAtaque
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public int BonusDefesa
+        {
+            get
+            {
+                return 2;
+            }
+        }
+    }
+}
