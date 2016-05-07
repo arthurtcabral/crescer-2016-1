f552d3fa9aa59b25ae3c5b4e8e0bd693872c5da8
Arthur Tassinari Cabral <Arthur Tassinari Cabral>
2016-05-07 18:18:40 -0300

    test(Nivel1Testes.cs): Testar novas funcionalidades exigidas pelo nível 2.

--- Nivel1Testes.cs
+++ Nivel1Testes.cs
@@ -8,7 +8,8 @@ namespace MegamanNivel1Testes
     [TestClass]
     public class Nivel1Testes
     {
-        
+        public IUpgrade CanhaoDePlasma { get; }
+
         [TestMethod]
         public void MegamanDeveTerMegamanComoNome()
         {
@@ -149,6 +150,162 @@ namespace MegamanNivel1Testes
             megaman.Atacar(protoman);
             protoman.Atacar(megaman);
             Assert.AreEqual(93, megaman.Vida);
+        }
+
+        [TestMethod]
+        public void CanhaoDePlasmaDaBonusDe2ParaAtaque()
+        {
+            IUpgrade iup = new CanhaoDePlasma();
+            Assert.AreEqual(2, iup.BonusAtaque);
+            Assert.AreEqual(0, iup.BonusDefesa);
+        }
+
+        [TestMethod]
+        public void EscudoDeEnergiaDaBonusDe2ParaDefesa()
+        {
+            IUpgrade iup = new EscudoDeEnergia();
+            Assert.AreEqual(0, iup.BonusAtaque);
+            Assert.AreEqual(2, iup.BonusDefesa);
+        }
+
+        [TestMethod]
+        public void BotasDeSuperVelocidadeDaBonusDe1ParaAtaqueEDe1ParaDefesa()
+        {
+            IUpgrade iup = new BotasDeSuperVelocidade();
+            Assert.AreEqual(1, iup.BonusAtaque);
+            Assert.AreEqual(1, iup.BonusDefesa);
+        }
+
+        [TestMethod]
+        public void MegamanUsaCanhaoDePlasma()
+        {
+            IUpgrade iup1 = new CanhaoDePlasma();
+            var megaman = new Megaman();
+            var bot = new Bot();
+
+            megaman.EquiparUpgrade(iup1);
+            megaman.Atacar(bot);
+            Assert.AreEqual(92, bot.Vida);
+        }
+
+        [TestMethod]
+        public void MegamanUsaEscudoDeEnergia()
+        {
+            IUpgrade iup1 = new EscudoDeEnergia();
+            var megaman = new Megaman();
+            var bot = new Bot();
+
+            megaman.EquiparUpgrade(iup1);
+            bot.Atacar(megaman);
+            Assert.AreEqual(97, megaman.Vida);
+        }
+
+        [TestMethod]
+        public void MegamanUsaBotasDeSuperVelocidade()
+        {
+            IUpgrade iup1 = new BotasDeSuperVelocidade();
+            var megaman = new Megaman();
+            var bot = new Bot();
+
+            megaman.EquiparUpgrade(iup1);
+            megaman.Atacar(bot);
+            Assert.AreEqual(93, bot.Vida);
+            bot.Atacar(megaman);
+            Assert.AreEqual(96, megaman.Vida);
+        }
+        
+        [TestMethod]
+        public void MegamanUsaTudo()
+        {
+            IUpgrade iup1 = new CanhaoDePlasma();
+            IUpgrade iup2 = new EscudoDeEnergia();
+            IUpgrade iup3 = new BotasDeSuperVelocidade();
+            var megaman = new Megaman();
+            var bot = new Bot();
+
+            megaman.EquiparUpgrade(iup1);
+            megaman.EquiparUpgrade(iup2);
+            megaman.EquiparUpgrade(iup3);
+            megaman.Atacar(bot);
+            Assert.AreEqual(91, bot.Vida);
+            bot.Atacar(megaman);
+            Assert.AreEqual(98, megaman.Vida);
+        }
+
+        [TestMethod]
+        public void RobosNaoPodemUsarMaisDe3Upgrades()
+        {
+            IUpgrade iup1 = new CanhaoDePlasma();
+            IUpgrade iup2 = new EscudoDeEnergia();
+            IUpgrade iup3 = new BotasDeSuperVelocidade();
+            IUpgrade iup4 = new CanhaoDePlasma();
+            var megaman = new Megaman();
+            var bot = new Bot();
+
+            megaman.EquiparUpgrade(iup1);
+            megaman.EquiparUpgrade(iup2);
+            megaman.EquiparUpgrade(iup3);
+            megaman.EquiparUpgrade(iup4); // Esse Upgrade não vai alterar o resultado. Resultado igual ao do teste anterior
+            megaman.Atacar(bot);
+            Assert.AreEqual(91, bot.Vida);
+            bot.Atacar(megaman);
+            Assert.AreEqual(98, megaman.Vida);
+        }
+
+        [TestMethod]
+        public void ProtomanUsaCanhaoDePlasma()
+        {
+            IUpgrade iup1 = new CanhaoDePlasma();
+            var megaman = new Protoman();
+            var bot = new Bot();
+
+            megaman.EquiparUpgrade(iup1);
+            megaman.Atacar(bot);
+            Assert.AreEqual(93, bot.Vida);
+        }
+
+        [TestMethod]
+        public void ProtomanUsaEscudoDeEnergia()
+        {
+            IUpgrade iup1 = new EscudoDeEnergia();
+            var megaman = new Protoman();
+            var bot = new Bot();
+
+            megaman.EquiparUpgrade(iup1);
+            bot.Atacar(megaman);
+            Assert.AreEqual(99, megaman.Vida);
+        }
+
+        [TestMethod]
+        public void ProtomanUsaBotasDeSuperVelocidade()
+        {
+            IUpgrade iup1 = new BotasDeSuperVelocidade();
+            var megaman = new Protoman();
+            var bot = new Bot();
+
+            megaman.EquiparUpgrade(iup1);
+            megaman.Atacar(bot);
+            Assert.AreEqual(94, bot.Vida);
+            bot.Atacar(megaman);
+            Assert.AreEqual(98, megaman.Vida);
+        }
+
+        [TestMethod]
+        public void ProtomanUsaTudo()
+        {
+            IUpgrade iup1 = new CanhaoDePlasma();
+            IUpgrade iup2 = new EscudoDeEnergia();
+            IUpgrade iup3 = new BotasDeSuperVelocidade();
+            var megaman = new Protoman();
+            var bot = new Bot();
+
+            megaman.EquiparUpgrade(iup1);
+            megaman.EquiparUpgrade(iup2);
+            megaman.EquiparUpgrade(iup3);
+            megaman.Atacar(bot);
+            Assert.AreEqual(92, bot.Vida);
+            bot.Atacar(megaman);
+            Assert.AreEqual(100, megaman.Vida);
         }
     }
 }
