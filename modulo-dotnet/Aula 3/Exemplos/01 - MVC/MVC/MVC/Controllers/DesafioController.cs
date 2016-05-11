424f52ae7539c1de0b279dd70774b85a98bccd88
Arthur Tassinari Cabral <Arthur Tassinari Cabral>
2016-05-11 03:24:17 -0300

    feat(Exercício 2): Adicionar desafio css dinâmico.

---
+++ modulo-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Controllers/DesafioController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class DesafioController : Controller
+    {
+
+        public ActionResult DesafioAnime(string aluno)
+        {
+            ViewBag.Path = ("~/Content/Estilos/" + aluno + ".css");
+            return View();
+        }
+    }
+}
