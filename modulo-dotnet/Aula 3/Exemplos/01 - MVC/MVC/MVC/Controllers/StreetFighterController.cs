bc38a579e4f9ee726f92137a2666bc4c8de6a777
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-05-11 13:57:39 -0300

    refactor(SobreMimModel.cs): Utilizar Model para todos os campos da página SobreMim.

--- modulo-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Controllers/StreetFighterController.cs
+++ modulo-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Controllers/StreetFighterController.cs
@@ -20,9 +20,24 @@ namespace MVC.Controllers
         public ActionResult SobreMim()
         {
             var arthur = new SobreMimModel();
-            arthur.altura = 1.91;
-            arthur.peso = 85;
-            arthur.medidas = "O que importa é que sou alto e magro.";
+            arthur.PrimeiraAparicao = "Hospital Moinhos de Vento.";
+            arthur.Nascimento = "25 de setembro de 1996.";
+            arthur.Altura = 1.91;
+            arthur.Peso = 85;
+            arthur.Medidas = "O que importa é que sou alto e magro.";
+            arthur.TipoSanguineo = "Um que os mosquitos gostam.";
+            arthur.Habilidades = "Estudar.";
+            arthur.Gosta = "Família, amigos, Inter, Subway";
+            arthur.Desgosta = "Ver que a térmica está virada.";
+            arthur.EstiloDeLuta = "#SemViolencia";
+            arthur.Origem = "Rio Grande do Sul, Tchê.";
+            arthur.FalaDeVitoria = "Copei!";
+            arthur.Nick1 = "CRCH25";
+            arthur.Nick2 = "boaarthur";
+            arthur.Nick3 = "Woody";
+            arthur.Stage1 = "Esteio - Cidade Maravilhosa, Brasil (Local randômico).";
+            arthur.Stage2 = "Esteio (Esteio).";
+            arthur.Golpes = "Corte de calcanhar, Chute de três dedos.";
             return View(arthur);
         }
     }
--- modulo-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Models/SobreMimModel.cs
+++ modulo-dotnet/Aula 3/Exemplos/01 - MVC/MVC/MVC/Models/SobreMimModel.cs
@@ -7,8 +7,24 @@ namespace MVC.Models
 {
     public class SobreMimModel
     {
-        public double altura { get; set; }
-        public int peso { get; set; }
-        public string medidas { get; set; }
+        public string PrimeiraAparicao { get; set; }
+        public string Nascimento { get; set; }
+        public double Altura { get; set; }
+        public int Peso { get; set; }
+        public string Medidas { get; set; }
+        public string TipoSanguineo { get; set; }
+        public string Habilidades { get; set; }
+        public string Gosta { get; set; }
+        public string Desgosta { get; set; }
+        public string EstiloDeLuta { get; set; }
+        public string Origem { get; set; }
+        public string FalaDeVitoria { get; set; }
+        public string Nick1 { get; set; }
+        public string Nick2 { get; set; }
+        public string Nick3 { get; set; }
+        public string Stage1 { get; set; }
+        public string Stage2 { get; set; }
+        public string Golpes { get; set; }
+
     }
 }
