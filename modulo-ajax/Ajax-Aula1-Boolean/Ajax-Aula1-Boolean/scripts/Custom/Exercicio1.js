1bd23235704041c94d6b55119b12301fe516bce5
Arthur Tassinari Cabral <Arthur Tassinari Cabral>
2016-05-20 01:38:23 -0300

    refactor(Exercicio1.js): Alterar nome do arquivo.

---
+++ modulo-ajax/Ajax-Aula1-Boolean/Ajax-Aula1-Boolean/scripts/Custom/Exercicio1.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+$(function () {
+    $.ajax({ url: 'https://api.spotify.com/v1/artists/6mdiAmATAx73kdxrNrnlao/albums?limit=50', type: 'GET' })
+.done(function (res) {
+    console.log(res.items);
+    var $musicas = $('#musicas');
+    res.items.forEach(function (album) {
+        album.images.forEach(function (imagem) {
+            var $img = $('<img>').attr('src', imagem.url);
+            if (imagem.height == 300) {
+                $musicas.append(
+                $('<li>').append($img)
+                );
+            }
+        });
+    });
+});
+});
+
+
