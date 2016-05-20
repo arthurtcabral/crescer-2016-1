cbf88b223cfaa36d8ab512cf72f05f6a62a4ab1e
Arthur Tassinari Cabral <Arthur Tassinari Cabral>
2016-05-20 02:01:17 -0300

    refactor(Exercício2.js): Adicionar lógica e estrutura para o exercício 3.

--- modulo-ajax/Ajax-Aula1-Boolean/Ajax-Aula1-Boolean/scripts/Custom/Exercicio2.js
+++ modulo-ajax/Ajax-Aula1-Boolean/Ajax-Aula1-Boolean/scripts/Custom/Exercicio2.js
@@ -1,15 +1,22 @@
 ﻿'use strict'
 
 $(function () {
+    var porcentagem = Math.floor(Math.random() * 11);
     var $ecletica = $("#ecletica");
     var $formEcletico = $('#form-ecletico');
     $formEcletico.submit(function (e) {
         var $termoPesquisa = $('input[name=pesquisa]').val();
+        var douchebag = ($termoPesquisa == 'Justin Bieber' && porcentagem > 2);
         var urlPesquisaPorId = "https://api.spotify.com/v1/search?q=" + $termoPesquisa + "&type=artist";
         $.ajax({ url: urlPesquisaPorId, type: 'GET' })
         .done(function (res) {
             var $ecletica = $('#ecletica');
-            var idBusca = res.artists.items[0].id;
+            var idBusca = null;
+            if (douchebag) {
+                idBusca = "douchebag";
+            } else {
+                idBusca = res.artists.items[0].id;
+            }
             var urlAlbum = "https://api.spotify.com/v1/artists/" + idBusca + "/albums?limit=50";
             $.ajax({ url: urlAlbum, type: 'GET' })
 .done(function (banda) {
@@ -23,6 +30,8 @@ $(function () {
             }
         });
     });
+}).fail(function (a) {
+    alert("Caro usuário, devido a um erro 400, não foi possível pesquisar por Justin Bieber");
 });
         });
         e.preventDefault();
