dd3f1b80dca8a4ceca13a0dca92d94f9bce85e5d
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-05-23 15:48:47 -0300

    feat(DateTimeExtensions.cs): adicionar extensão conversora para DateTime.

---
+++ modulo-ajax/CdZ/src/CdZ.MVC/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace CdZ.MVC.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string FromDateTimeToISOString(this DateTime date)
+        {
+            return date.ToString("s", CultureInfo.InvariantCulture);
+        }
+    }
+}
