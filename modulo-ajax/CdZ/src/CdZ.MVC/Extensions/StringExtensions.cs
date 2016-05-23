720f83e75b4866fad77a6222313cbe3e06f26c6d
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-05-23 15:53:33 -0300

    feat(StringExtensions.cs): adicionar classe de extensão que converte string em DateTime.

---
+++ modulo-ajax/CdZ/src/CdZ.MVC/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace CdZ.MVC.Extensions
+{
+    public static class StringExtensions
+    {
+        public static DateTime FromISOToDateTime(this string isoDate)
+        {
+            return DateTime.Parse(isoDate, null, DateTimeStyles.RoundtripKind);
+        }
+    }
+}
