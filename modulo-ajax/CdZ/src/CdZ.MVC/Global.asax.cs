cd89fddcfafbb78251af306d79d83c475e0586d2
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-05-23 16:26:00 -0300

    fix(Global.asax.cs): adicionar aplicabilidade para ModelBinders.

--- modulo-ajax/CdZ/src/CdZ.MVC/Global.asax.cs
+++ modulo-ajax/CdZ/src/CdZ.MVC/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CdZ.MVC.CustomModelBinders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,8 @@ namespace CdZ.MVC
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ModelBinders.Binders.Add(typeof(double), new DoubleModelBinder());
+            ModelBinders.Binders.Add(typeof(double?), new DoubleModelBinder());
         }
     }
 }
