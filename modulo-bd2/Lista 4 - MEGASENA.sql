945fdc85de647cb919924c6476d751a78f5f382d
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-06-02 17:20:17 -0300

    feat(Lista 4 - MEGASENA.sql): adicionar esboço de busca por ganhadores da quadra.

---
+++ modulo-bd2/Lista 4 - MEGASENA.sql
@@ -0,0 +1,74 @@
+SELECT * FROM ARQUIVO_APOSTA_A where n1 = 5 and n2 = 10 and n3 = 12 and n4 = 22 and n5 = 28 and n6 = 48;
+
+SELECT * FROM ARQUIVO_APOSTA_A where 
+(n1 = 5 AND n2 = 10 AND n3 = 12 AND n4 = 22)
+OR
+(n2 = 5 AND n3 = 10 AND n4 = 12 AND n5 = 22)
+OR
+(n3 = 5 AND n4 = 10 AND n5 = 12 AND n6 = 22)
+OR
+(n4 = 5 AND n5 = 10 AND n6 = 12 AND n7 = 22)
+OR
+(n5 = 5 AND n6 = 10 AND n7 = 12 AND n8 = 22)
+OR
+(n6 = 5 AND n7 = 10 AND n8 = 12 AND n9 = 22)
+OR
+(n7 = 5 AND n8 = 10 AND n9 = 12 AND n10 = 22)
+OR
+(n8 = 5 AND n9 = 10 AND n10 = 12 AND n11 = 22)
+OR
+(n9 = 5 AND n10 = 10 AND n11 = 12 AND n12 = 22)
+OR
+(n10 = 5 AND n11 = 10 AND n12 = 12 AND n13 = 22)
+OR
+(n11 = 5 AND n12 = 10 AND n13 = 12 AND n14 = 22)
+OR
+(n12 = 5 AND n13 = 10 AND n14 = 12 AND n15 = 22)
+OR
+(n1 = 10 AND n2 = 12 AND n3 = 22 AND n4 = 28)
+OR
+(n2 = 10 AND n3 = 12 AND n4 = 22 AND n5 = 28)
+OR
+(n3 = 10 AND n4 = 12 AND n5 = 22 AND n6 = 28)
+OR
+(n4 = 10 AND n5 = 12 AND n6 = 22 AND n7 = 28)
+OR
+(n5 = 10 AND n6 = 12 AND n7 = 22 AND n8 = 28)
+OR
+(n6 = 10 AND n7 = 12 AND n8 = 22 AND n9 = 28)
+OR
+(n7 = 10 AND n8 = 12 AND n9 = 22 AND n10 = 28)
+OR
+(n8 = 10 AND n9 = 12 AND n10 = 22 AND n11 = 28)
+OR
+(n9 = 10 AND n10 = 12 AND n11 = 22 AND n12 = 28)
+OR
+(n10 = 10 AND n11 = 12 AND n12 = 22 AND n13 = 28)
+OR
+(n11 = 10 AND n12 = 12 AND n13 = 22 AND n14 = 28)
+OR
+(n12 = 10 AND n13 = 12 AND n14 = 22 AND n15 = 28)
+OR
+(n1 = 12 AND n2 = 22 AND n3 = 28 AND n4 = 48)
+OR
+(n2 = 12 AND n3 = 22 AND n4 = 28 AND n5 = 48)
+OR
+(n3 = 12 AND n4 = 22 AND n5 = 28 AND n6 = 48)
+OR
+(n4 = 12 AND n5 = 22 AND n6 = 28 AND n7 = 48)
+OR
+(n5 = 12 AND n6 = 22 AND n7 = 28 AND n8 = 48)
+OR
+(n6 = 12 AND n7 = 22 AND n8 = 28 AND n9 = 48)
+OR
+(n7 = 12 AND n8 = 22 AND n9 = 28 AND n10 = 48)
+OR
+(n8 = 12 AND n9 = 22 AND n10 = 28 AND n11 = 48)
+OR
+(n9 = 12 AND n10 = 22 AND n11 = 28 AND n12 = 48)
+OR
+(n10 = 12 AND n11 = 22 AND n12 = 28 AND n13 = 48)
+OR
+(n11 = 12 AND n12 = 22 AND n13 = 28 AND n14 = 48)
+OR
+(n12 = 12 AND n13 = 22 AND n14 = 28 AND n15 = 48);
