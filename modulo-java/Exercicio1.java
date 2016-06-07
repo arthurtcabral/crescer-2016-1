9aeddc52d3acb8ba4326fb12493ad0f93dc7b345
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-06-07 17:04:38 -0300

    feat(Exercicio1.java): adicionar exercício de verificação de paridade.

---
+++ modulo-java/Exercicio1.java
@@ -0,0 +1,20 @@
+﻿package br.com.crescer;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Informe um número: ");
+        int num = teclado.nextInt();
+        
+        if(num % 2 == 0){
+            System.out.println("Par");
+        }else{
+            System.out.println("Ímpar");
+        }
+    }
+    
+}
