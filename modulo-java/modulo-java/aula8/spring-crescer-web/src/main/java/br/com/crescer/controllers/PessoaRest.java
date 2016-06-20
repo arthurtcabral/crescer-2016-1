dcb93c8696fa2eb4bf4dd5cd422c0daa9523cae2
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-06-20 16:56:19 -0300

    refactor(PessoaRest.java): alterar PessoaRest de modo que fique ajustado ao uso da paginação.

--- modulo-java/modulo-java/aula8/spring-crescer-web/src/main/java/br/com/crescer/controllers/PessoaRest.java
+++ modulo-java/modulo-java/aula8/spring-crescer-web/src/main/java/br/com/crescer/controllers/PessoaRest.java
@@ -4,6 +4,8 @@ import br.com.crescer.services.PessoaService;
 import br.com.crescer.entity.Pessoa;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,8 +22,8 @@ public class PessoaRest {
     PessoaService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public Iterable<Pessoa> list() {
-        return service.findAll();
+    public Page<Pessoa> list(Pageable pgbl) {
+        return service.findAll(pgbl);
     }
 
     @RequestMapping(value = "/save_or_update", method = RequestMethod.POST)
