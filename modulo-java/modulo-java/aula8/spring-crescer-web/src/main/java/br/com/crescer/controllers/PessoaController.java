843f47494df466678fa9d9ecbe49d0ed6587480e
Arthur Tassinari Cabral <arthur.cabral@cwinet.local>
2016-06-20 16:55:47 -0300

    fix(PessoaController.java): alterar PessoaController de modo a ajustar à implementação da segurança.

--- modulo-java/modulo-java/aula8/spring-crescer-web/src/main/java/br/com/crescer/controllers/PessoaController.java
+++ modulo-java/modulo-java/aula8/spring-crescer-web/src/main/java/br/com/crescer/controllers/PessoaController.java
@@ -4,12 +4,13 @@ import br.com.crescer.entity.Pessoa;
 import br.com.crescer.services.PessoaService;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/pessoa")
@@ -17,24 +18,23 @@ public class PessoaController {
 
     @Autowired
     PessoaService service;
-
+    
     @RequestMapping(method = RequestMethod.GET)
-    public String list(Model model, @RequestParam(required = false) Long id) {
-        if (id != null) {
-            service.delete(id);
-        }
+    public String list(Model model, Pageable pageable) {
+        Page<Pessoa> pessoas = service.findAll(pageable);
+        model.addAttribute("pessoas", pessoas);
         model.addAttribute("pessoa", new Pessoa());
-        model.addAttribute("pessoas", service.findAll());
         return "index";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String save(@ModelAttribute Pessoa p, Model model) {
+    public String save(Pessoa p) {
         if (p.getNascimento() == null) {
             p.setNascimento(new Date());
         }
         service.save(p);
-        return list(model, null);
+//        return list(model, null);
+return "redirect:/pessoa";
     }
 
 }
